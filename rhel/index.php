7e6baad6a0de09f4543208028f4d28a3209e3cad
Donny Davis <donny@cloudspin.me>
2016-01-27 18:16:08 -0500

    updated index

--- rhel/index.php
+++ rhel/index.php
@@ -3,4 +3,10 @@ echo "<h1>Hello, World!</h1>";
 $serverIP = $_SERVER['SERVER_ADDR'];
 echo date('Y-m-d H:i:s');
 echo "<p>Server IP is: <b>{$serverIP}</b></p>";
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
 ?>
